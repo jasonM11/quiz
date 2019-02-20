42b466841898ff3c3e08ee2d45a29fa03c4dd147
Jason Mejia <mejiajason640@gmail.com>
2019-02-20 20:15:01 +0000

    totalScore

--- js/script.js
+++ js/script.js
@@ -5,29 +5,61 @@ $(document).ready(function() {
         var name = $("#name").val();
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
-        var q1Result = $("#question1").val();
-        var q2Result = $("#question2").val();
-        var q3Result = $("#question3").val();
-        console.log("here");
-        var totalScore;
-    });
+        var q1Input = $("#question1").val();
+        var q2Input = $("#question2").val();
+        var q3Input = $("#question3").val();
+        
+        var q1Scores = questionOne(q1Input);
+        var q2Scores = questionTwo(q2Input);
+        var q3Scores = questionThree(q3Input);
+        var totalScore= q1Scores+q2Scores+q3Scores;
+        $(".result").html(totalScore);
+});
 });
 
-
 function questionOne(answer){
-    if (answer==="basketball") {
+    if (answer==="Reading") {
         return 1
     }
-    else if (answer==="swimming") {
+    else if (answer==="Coding") {
         return 2
     }
-    else if (answer==="reading") {
+    else if (answer==="Sports") {
         return 3
     }
     else {
         return 0
     }
 }
+function questionTwo(answer){
+    if (answer==="Pancakes") {
+        return 1
+    }
+    else if (answer==="Waffles"){
+        return 2
+    }
+    else if (answer==="French Toast"){
+        return 3
+    }
+    else {
+        return 0
+    }
+}
+function questionThree(answer){
+    if (answer.length<4){
+        return 1
+    }
+    else if (answer.length<8){
+        return 2
+    }
+    else if (answer.length<12){
+        return 3
+    }
+    else {
+        return 0
+    }
 
-console.log(questionOne("swimming"))
+
+    
+}
 
